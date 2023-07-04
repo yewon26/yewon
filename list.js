fe5ab24a5dfee98c6a408a8b4a81f27a5d5afd5e
임예원 <dla2238@naver.com>
2023-07-04 10:23:49 +0900

    작업 초기화 하고 html, js 커밋

---
+++ list.js
@@ -0,0 +1,35 @@
+const API='http://sample.bmaster.kro.kr/contacts';
+
+function getPageno (){
+  const params = new URLSearchParams(location.search);
+  return params.get('pageno')===null? 1: params.get('pageno');
+}
+async function fetch(pageno, pagesize=10){
+  const url = `${API}?pageno=?${pageno}&pagesize=${pagesize}`;
+  try{
+    return await $.ajax(url);
+  }catch(err){
+    console.log(err);
+    return null;
+  }
+}
+function printContacts(contacts){
+  const $tbody = $.ajax('#tbody');
+  for(const c of contacts){
+    const html = `
+    <tr>
+    <td>${c.no}</td>
+    <td>${c.name}</td>
+    <td>${c.address}</td>
+    <td>${c.tel}</td>
+    </tr>
+    `;
+    $tbody.append(html);
+  }
+}
+function getPagination(){
+
+}
+function printPagination(){
+
+}
